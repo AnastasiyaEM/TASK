ddecb4faec2e1db7d07d08c5aa54038949ebd62a
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-11-12 01:59:47 +0300

    Task_45

---
+++ ДЗ9/Task_45/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+string NaturNumber(int N, int M)
+{
+    if (N>=M) return NaturNumber(N -1, M) + $"{N} ";
+    else return " ";
+}
+ 
+
+Console.WriteLine(" Введите число M (от)");
+int M = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine(" Введите число N (до)");
+int N = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine($" Натуральные числа: {NaturNumber(N,M)}");
