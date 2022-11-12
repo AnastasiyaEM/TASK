224cd265c4483842438f4a5fa11b8f925a7d183a
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-11-12 21:36:28 +0300

    Task_47

---
+++ ДЗ9/Task_47/Program.cs
@@ -0,0 +1,22 @@
+﻿//Написать программу вычисления функции Аккермана
+
+// функция Аккермана принимает 2 неотрицательных числа 
+
+int Akkerman (int n, int z)
+{
+    if (n == 0) return z+1;
+    else if (n > 0 && z > 0) return Akkerman(n - 1, Akkerman(n, z-1));
+    else if (n !=0 && z == 0) return Akkerman(n - 1, 1);
+    return Akkerman(n,z);
+}
+
+int ReadIntegerNumber(string message)
+    {
+        Console.Write(message);
+        return int.Parse(Console.ReadLine() ?? "0");
+    }
+
+    int n = ReadIntegerNumber("Введите число n: ");
+    int z = ReadIntegerNumber("Введите число z: ");
+
+Console.WriteLine($"Вычисление значения функцией Аккермана: {Akkerman(n,z)}");
