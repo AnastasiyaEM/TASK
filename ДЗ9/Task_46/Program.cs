b939329c1195d92ac646e3929923a5a10aff5e67
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-11-12 22:04:30 +0300

    Task_46

---
+++ ДЗ9/Task_46/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int Sum(int M, int N)
+{
+    if (N == M) return 0; // возможность рекурсивного выхода
+    else if (N > M) return N + Sum(M,N -1);
+    return Sum(M,N);
+}
+
+
+Console.WriteLine(" Введите число M (от)");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine(" Введите число N (до)");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int sum = Sum(M,N);
+Console.WriteLine($"Сумма элементов от M до N: ");
+Console.WriteLine(sum);
