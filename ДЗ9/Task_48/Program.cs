4e257a16082a41442a7c61d5204f1062cc30022c
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-11-12 20:53:50 +0300

    Task_48

---
+++ ДЗ9/Task_48/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int Power(int A, int B)
+{
+    if (B == 0) return 1;
+    else return Power(A, B - 1) * A;
+}
+
+int ReadIntegerNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int A = ReadIntegerNumber("Введите число А: ");
+int B = ReadIntegerNumber("В какую степень возвести число А: ");
+
+Console.WriteLine(Power(A,B));
