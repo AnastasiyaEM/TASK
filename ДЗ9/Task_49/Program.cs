7722d1e163a13382c12365ca9b0b0e52dba17000
Емельянова Анастасия <emelanova@MacBook-Air-Emelanova.local>
2022-11-13 20:24:32 +0300

    Task_49

---
+++ ДЗ9/Task_49/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+//Первые два элемента последовательности задаются пользователем
+
+
+string Numbers( int n,int A, int E, int i =3)
+{
+    if (n !=0)
+    {
+    --n;
+    int sum = A+E; return sum + Numbers(n,A,E,i+1);
+    }
+    else return " ";
+}
+
+int ReadIntegerNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int A = ReadIntegerNumber("Введите первый эл-т последовательности: ");
+int E = ReadIntegerNumber("Введите второй эл-т последовательности: ");
+int n = ReadIntegerNumber("Сколько чисел показать? : ");
+Console.WriteLine (Numbers(n,A,E));
